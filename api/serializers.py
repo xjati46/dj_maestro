dbf6b15c6e14ee550c5d1902cce32feaf3b54b22
xjati46 <x_jati_46@yahoo.com>
2021-05-01 00:02:49 +0700

    modify serializer to convert foreignfield data id to expected data

--- api/serializers.py
+++ api/serializers.py
@@ -28,9 +28,11 @@ class ProdukSerializer(serializers.ModelSerializer):
         fields = ('id', 'nama_produk', 'harga', 'jumlah_pertemuan')
 
 class PelatihSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.username')
+
     class Meta:
         model = Pelatih
-        fields = ('id', 'user', 'nama_lengkap', 'nama_panggilan', 'jenis_kelamin', 'bagi_hasil')
+        fields = ('id', 'user_name', 'nama_lengkap', 'nama_panggilan', 'jenis_kelamin', 'bagi_hasil')
 
 class SiswaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,6 +40,10 @@ class SiswaSerializer(serializers.ModelSerializer):
         fields = ('id', 'nama_lengkap', 'nama_panggilan', 'jenis_kelamin', 'usia')
 
 class PesananSerializer(serializers.ModelSerializer):
+    nama_siswa = serializers.CharField(source='siswa.nama_lengkap')
+    nama_pelatih = serializers.CharField(source='pelatih.nama_lengkap')
+    nama_produk = serializers.CharField(source='produk.nama_produk')
+
     class Meta:
         model = Pesanan
-        fields = ('id', 'pelatih', 'arsip', 'siswa', 'produk', 'diskon', 'tgl_transaksi', 'tgl_habis', 'p1', 'p1_c', 'p2', 'p2_c', 'p3', 'p3_c', 'p4', 'p4_c', 'p5', 'p5_c', 'p6', 'p6_c', 'p7', 'p7_c', 'p8', 'p8_c', 'status_habis', 'nilai_transaksi', 'p_total', 'p_c_total', 'margin_p_c', 'honor_per_sesi', 'honor_pencairan')
+        fields = ('id', '__str__', 'nama_pelatih', 'arsip', 'nama_siswa', 'nama_produk', 'diskon', 'tgl_transaksi', 'tgl_habis', 'p1', 'p1_c', 'p2', 'p2_c', 'p3', 'p3_c', 'p4', 'p4_c', 'p5', 'p5_c', 'p6', 'p6_c', 'p7', 'p7_c', 'p8', 'p8_c', 'status_habis', 'nilai_transaksi', 'p_total', 'p_c_total', 'margin_p_c', 'honor_per_sesi', 'honor_pencairan')
